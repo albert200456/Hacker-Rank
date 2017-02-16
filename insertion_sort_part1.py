bc6f11320e9f5ded11c11d3afbab3729f8d30a8c
Kevin Lin <kevlin@Kevins-MacBook-Pro.local>
2017-02-16 00:04:10 -0600

    Insertion Sort commit

---
+++ insertion_sort_part1.py
@@ -0,0 +1,32 @@
+#!/bin/python
+def insertionSort(ar):  
+    for i in range(len(ar)):
+        current_value = ar[i]
+        pos = i
+        modYet = 0
+            
+        while pos > 0 and current_value < ar[pos - 1]:
+            modYet = 1
+            ar[pos] = ar[pos - 1]
+            pos -= 1
+
+            for i in range(len(ar) - 1):
+                print ar[i],
+            print ar[-1]
+
+            if current_value > ar[pos - 1]:
+                ar[pos] = current_value
+                for i in range(len(ar) - 1):
+                    print ar[i],
+                print ar[-1]
+         
+        if pos == 0 and modYet == 1:
+            ar[pos] = current_value
+            for i in range(len(ar) - 1):
+                print ar[i],
+            print ar[-1]
+
+m = input()
+ar = [int(i) for i in raw_input().strip().split()]
+insertionSort(ar)
+
